767b0a11e41e7e0833042fb6880ab9fbc1cee285
Tammo <dev@tammopape.de>
2012-07-04 19:43:50 +0200

    made some major improvements
    - moved _write to truly private
    - used a lot improvements from "essential js design patterns book" from
      addyosmani.com
    - changed qunit tests that the changes pass
    
    \#geeklist

--- jquery.lazygist.js
+++ jquery.lazygist.js
@@ -1,5 +1,5 @@
 /**
- * Lazygist v0.1
+ * Lazygist v0.1-beta
  *
  * a jQuery plugin that will lazy load your gists.
  *
@@ -7,28 +7,49 @@
  * https://github.com/tammo/jquery-lazy-gist
  *
  * Copyright, Tammo Pape
- * http://www.tammopape.de
+ * http://tammopape.de
  *
  * Licensed under the MIT license.
  */
 
-(function( $ ){
+;(function( $, window, document, undefined ){
+    "use strict";
+    
+    //
+    // note:
+    // this plugin is not stateful
+    // and will not communicate with own instances at different elements
+    //
 
-	var options;
+    var pluginName = "lazygist",
+    version = "0.1-beta",
+    
+    defaults = {
+        // adding the ?file parameter to choose a file
+        'url_template': 'https://gist.github.com/{id}.js?file={file}',
+        
+        // if these are strings, the attributes will be read from the element
+        'id': 'data-id',
+        'file': 'data-file'
+    },
+    
+	options,
 	
-	// will be temporarily replaced
-	var originwrite = document.write;
+	// will be replaced
+	originwrite = document.write,
 	
 	// stylesheet urls found in document.write calls
 	// they are cached to write them once to the document,
 	// not three times for three gists
-	var stylesheets = [];
+	stylesheets = [],
+    
+    // cache gist-ids to know which are already appended to the dom
+    ids_dom = [],
 	
-	// if lock has elements, there are write functions active
-	// this is used to restore the origin document.write function after all
-	var lock_stack = [];
-
-	var methods = {
+    // remember gist-ids if their javascript is already loaded
+    ids_ajax = [],
+    
+	methods = {
 		
 		/**
 		 * Standard init function
@@ -37,16 +58,17 @@
 		init : function( options_input ){
 		
 			// default options are default
-			options = $.extend({
-				
-				// add the ?file parameter to set a filename correctly
-				'url_template' : 'https://gist.github.com/{id}.js?file={file}',
-				
-				// if these are strings, the attributes will be read from the element
-				'id' : 'data-id',
-				'file' : 'data-file'
-			}, options_input);
+			options = $.extend({}, defaults, options_input);
 		
+            // can be reset
+            document.write = _write;
+            
+            $.each(options, function(index, value) {
+                if(typeof value !== "string") {
+                    throw new TypeError(value + " (" + (typeof value) + ") is not a string");
+                }
+            });
+            
 			return this.lazygist('load');
 		},
 		
@@ -54,81 +76,94 @@
 		 * Load the gists
 		 */
 		load : function() {
-			// (!) replace origin document.write function
-			//
-			// iterate over gist anchors
-			// (1) collect html by injecting script elements
-			// (2) append the gist-iframe through the new document.write func (see _write)
-			
-			document.write = methods['_write'];
-			
+			// (1) iterate over gist anchors
+			// (2) append the gist-html through the new document.write func (see _write)
+            
+            // (1)
 			return this.filter('[data-id]').each(function(){
-				// (1)
-				// there will be a _write call soon, so dont restore origin write
-				lock_stack.push(true);
 				
-				// todo: better checks
-				var id = $(this).attr(options.id);
-				var file = $(this).attr(options.file);
-				
-				if ( id != undefined ) {
+				var id = $(this).attr(options.id),
+                    file = $(this).attr(options.file),
+                    src;
+                
+				if( id !== undefined ) {
 					
-					var src = options.url_template.replace(/{id}/g, id).replace(/{file}/g, file);
+                    if( $.inArray(id, ids_ajax) !== -1 ) {
+                        // just do nothin, if gist is already ajaxed
+                        return;
+                    }
+                    
+                    ids_ajax.push(id);
+                    
+					src = options.url_template.replace(/\{id\}/g, id).replace(/\{file\}/g, file);
 					
-					// (2)
+					// (2) this will trigger our _write function
 					$.getScript(src);
-					
-					// here we think, that there will always be a succesfull ajax call,
-					// that will restore the origin write function (see _write) in the end
-					//
-					// but we should take care of errors (todo)
 				}
 			});
 		},
-		
-		/**
-		 * Special document.write function
-		 *
-		 * Filters the css file from github.com to add it to the head - once -
-		 *
-		 * Will replace itself after getting things done. This happens
-		 * to keep flexibility with other scripts as high as possible
-		 * (create a ticket if it messes things up!)
-		 */
-		_write : function( content ) {
-			
-			if( content.indexOf( 'rel="stylesheet"' ) != -1 ) {
-				var href = /href="(.*github.com.*.css)"/g.exec(content);
-				
-				if ( $.inArray(href[1], stylesheets) == -1 ) {
-							
-					$('head').append(content);
-					stylesheets.push(href[1]);
-				}
-				
-			} else if( content.indexOf( 'id="gist' ) != -1 ) {
-				var id = /gist-([\d]{1,})/g.exec(content);
-				
-				$('.gist_here[data-id=' + id[1] + ']').append(content);
-			
-			
-				// i am done with this _write func, release
-				var popped = lock_stack.pop();
-				
-				if(popped === undefined) {
-					// it is the last _write call
-					document.write = originwrite;
-				}
-				
-			} else {
-				// this is a fallback for interoperability
-				originwrite.apply( this, arguments );
-			}
-		}
+        
+        /**
+         * Just reset the write function
+         */
+        reset_write: function() {
+            document.write = originwrite;
+        }
 	};
-
+    
+    /**
+     * private special document.write function
+     *
+     * Filters the css file from github.com to add it to the head - once -
+     *
+     * It has a fallback to keep flexibility with other scripts as high as possible
+     * (create a ticket if it messes things up!)
+     *
+     * Keep in mind, that a call to this function happens after
+     * an ajax call by jQuery. One *cannot* know which gist-anchor
+     * to use. You can only read the id from the content.
+     */
+    function _write( content ) {
+        
+        var expression, // for regexp results
+            href, // from the url
+            id; // from the content
+        
+        if( content.indexOf( 'rel="stylesheet"' ) !== -1 ) {
+            expression = /(github\.com[a-z\/]*\.css)/g.exec(content);
+            href = expression[1];
+            
+            // check if stylesheet is already inserted
+            if ( $.inArray(href, stylesheets) === -1 ) {
+                
+                $('head').append(content);
+                stylesheets.push(href);
+            }
+            
+        } else if( content.indexOf( 'id="gist' ) !== -1 ) {
+            expression = /gist-([\d]{1,})/g.exec(content);
+            id = expression[1];
+            
+            if( id !== undefined ) {
+            
+                // test if id is already loaded
+                if( $.inArray(id, ids_dom) !== -1 ) {
+                    // just do nothin, if gist is already attached to the dom
+                    return;
+                }
+                
+                ids_dom.push(id);
+                
+                $('.gist_here[data-id=' + id + ']').append(content);
+            }
+        } else {
+            // this is a fallback for interoperability
+            originwrite.apply( document, arguments );
+        }
+    }
+    
 	// method invocation - from jQuery.com
-	$.fn.lazygist = function( method ) {
+	$.fn[pluginName] = function( method ) {
 	
 		if ( methods[method] ) {
 			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
@@ -140,5 +175,8 @@
 			$.error( 'Method ' +  method + ' does not exist on jQuery.lazygist' );
 		}    
 	};
+    
+    // expose version for your interest
+    $.fn[pluginName].version = version;
 
-})(jQuery);
+})(jQuery, window, document);
--- test/qunit.lazygist.js
+++ test/qunit.lazygist.js
@@ -1,6 +1,9 @@
+
 module("lazygist");
 
-var originScript = jQuery.getScript;
+var originwrite = document.write,
+    originScript = jQuery.getScript;
+    
 // stub getScript, to not bother github.com
 var getScriptStub = function(src) {
     console.log(src);
@@ -26,8 +29,33 @@ test("no parameters set", function() {
     } catch(e) {}
 });
 
-test("jQuery method calling", function() {
-    expect(2);
+test("wrong parameters", function() {
+    expect(3);
+    
+    raises(function() {
+        jQuery().lazygist({
+            id: function() {}
+        });
+        
+    }, "id option should be forced to be a string");
+    
+    raises(function() {
+        jQuery().lazygist({
+            file: function() {}
+        });
+        
+    }, "file option should be forced to be a string");
+
+    raises(function() {
+        jQuery().lazygist({
+            url_template: function() {}
+        });
+        
+    }, "url_template option should be forced to be a string");
+});
+
+test("plugin method calling", function() {
+    expect(3);
     
     try {
         jQuery().lazygist("init");
@@ -38,6 +66,11 @@ test("jQuery method calling", function() {
         jQuery().lazygist("invalid");
         
     }, "must throw error if method is invalid");
+    
+    raises(function() {
+        jQuery().lazygist("_write");
+        
+    }, "must throw error on call to private _write function");
 });
 
 test("chainability", function() {
@@ -53,21 +86,23 @@ test("chainability", function() {
     markup = jQuery("<div data-id='1'></div><p></p>");
     list = markup.lazygist();
     
-    equal(1, list.length, "should return one element");
+    equal(list.length, 1, "should return one element");
     
     //-------
     
     markup = jQuery("<div></div><p></p>");
     list = markup.lazygist();
     
-    equal(0, list.length, "should return no element");
+    equal(list.length, 0, "should return no element");
     
     //-------
     
     markup = jQuery("<div data-id='1'></div><div data-id='1'></div><div data-id='1'></div><div data-id='1'></div>");
     list = markup.lazygist();
     
-    equal(4, list.length, "should return four elements");
+    equal(list.length, 4, "should return four elements");
+    
+    markup.remove();
 });
 
 asyncTest("attribute reading", function() {
@@ -88,7 +123,8 @@ asyncTest("attribute reading", function() {
     var markup = jQuery("<div data-id='1472' data-file='my file'></div>");
     markup.lazygist();
     
-    jQuery.getScript = getScriptStub;
+    markup.remove();
+    jQuery.getScript = originScript;
 });
 
 asyncTest("attribute reading 2", function() {
@@ -109,7 +145,8 @@ asyncTest("attribute reading 2", function() {
     var markup = jQuery("<div data-id='1472a!' data-file='my%20file%'></div>");
     markup.lazygist();
     
-    jQuery.getScript = getScriptStub;
+    markup.remove();
+    jQuery.getScript = originScript;
 });
 
 asyncTest("attribute reading 3", function() {
@@ -121,16 +158,17 @@ asyncTest("attribute reading 3", function() {
         
         var expression = /.*github\.com\/(.*)\.js\?file=(.*)/g.exec(src);
         
-        equal(expression[1], "1472", "id should be 1472a!");
+        equal(expression[1], "14725", "id should be 14725");
         equal(expression[2], "undefined", "file should be undefined");
         
         start();
     };
     
-    var markup = jQuery("<div data-id='1472'></div>");
+    var markup = jQuery("<div data-id='14725'></div>");
     markup.lazygist();
     
-    jQuery.getScript = getScriptStub;
+    markup.remove();
+    jQuery.getScript = originScript;
 });
 
 test("stylesheet append", function() {
@@ -143,10 +181,134 @@ test("stylesheet append", function() {
         document.write('<link rel="stylesheet" href="https://gist.github.com/stylesheets/gist/embed.css"/>');
     };
     
-    var markup = jQuery("<div data-id='1'></div>");
+    var markup = jQuery("<div data-id='123'></div>");
     markup.lazygist();
     
-    jQuery.getScript = getScriptStub;
+    equal($("head link[rel=stylesheet]").length, 2, "there should be 2 stylesheets");
     
-    equal(jQuery('head link[rel=stylesheet]').length, 2, "there should be two stylesheets");
+    markup.remove();
+    jQuery.getScript = originScript;
+});
+
+test("html append", function() {
+    expect(1);
+    
+    // stub getScript, to check url values
+    jQuery.getScript = function(src) {
+        getScriptStub(src);
+        
+        document.write('<div id=\"gist-2942256\" class=\"gist\">\n\n    ');
+    };
+    
+    var markup = jQuery("<div class='gist_here' data-id='2942256'></div>"),
+        body = jQuery("body").append(markup);
+    
+    var expected = jQuery(".gist_here").lazygist(),
+        result = jQuery('#gist-2942256');
+    
+    equal(result.length, 1, "should insert one gist html");
+    
+    jQuery('.gist_here').remove();
+    jQuery.getScript = originScript;
+});
+    
+test("html append 2", function() {
+    expect(1);
+    
+    // stub getScript, to check url values
+    jQuery.getScript = function(src) {
+        getScriptStub(src);
+        
+        document.write('<div id=\"gist-294\" class=\"gist\">\n\n    ');
+    };
+    
+    var markup = jQuery("<div class='gist_here' data-id='294'></div>"),
+        markup2 = jQuery("<div class='gist_here' data-id='294'></div>"),
+        body = jQuery("body").append(markup).append(markup2);
+    
+    var expected = jQuery(".gist_here").lazygist(),
+        result = jQuery('[id^="gist-"]');
+        
+    equal(result.length, 2, "should insert two gist html");
+    
+    jQuery('.gist_here').remove();
+    jQuery.getScript = originScript;
+});
+
+test("html append 3", function() {
+    expect(1);
+    
+    var count = 0;
+    
+    // stub getScript, to check url values
+    jQuery.getScript = function(src) {
+        getScriptStub(src);
+        
+        count++;
+    };
+    
+    var markup = jQuery("<div class='gist_here' data-id='42256'></div>"),
+        markup2 = jQuery("<div class='gist_here' data-id='42256'></div>"),
+        body = jQuery("body").append(markup).append(markup2);
+    
+    var expected = jQuery(".gist_here").lazygist();
+    
+    equal(count, 1, "for one gist-id there should be one ajax call");
+    
+    jQuery('.gist_here').remove();
+    jQuery.getScript = originScript;
+});
+
+test("already loaded", function() {
+    expect(1);
+    
+    var count = 0;
+    
+    // stub getScript, to count calls
+    jQuery.getScript = function(src) {
+        getScriptStub(src);
+        
+        count++;
+    };
+    
+    var markup = jQuery("<div data-id='47'></div>");
+    markup.lazygist();
+    markup.lazygist();
+    markup.lazygist();
+    markup.lazygist();
+    
+    equal(count, 1, "load gist from same element only once");
+    
+    markup.remove();
+});
+
+asyncTest("restoring of original write function", function() {
+    expect(1);
+    
+    var markup = jQuery("<div data-id='4698'></div>");
+    markup.lazygist();
+    markup.lazygist('reset_write');
+    
+    strictEqual(document.write, originwrite);
+    
+    markup.remove();
+    start();
+});
+
+test("jQuery method calls", function() {
+    expect(1);
+
+    // stub getScript, to count calls
+    jQuery.getScript = function(src) {
+        getScriptStub(src);
+        
+        ok(true, "plugin should use jQuery getScript function");
+    };
+    
+    var markup = jQuery('<div data-id="486"></div><p></p>');
+    
+    markup.lazygist();
+    
+    markup.remove();
+    jQuery.getScript = originScript;
 });
